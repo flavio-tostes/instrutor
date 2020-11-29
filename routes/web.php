32e720519ea9e7c35266a5d385f2c319980c3f99
Flavio Tostes <fctostes@gmail.com>
2020-11-29 10:45:39 -0300

    Alteração de teste

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
